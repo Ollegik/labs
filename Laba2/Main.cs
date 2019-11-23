2bd73e47f35cb578c3996f2a823f59cc3eb686ba
Ollegik <oleg.kozinov@mail.ru>
2019-11-23 17:08:09 +0300

    2 lab
    create

---
+++ Laba2/Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Figures;
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Козинов Олег ИУ5-35Б";
+
+            Rectangle rectangle = new Rectangle(10, 20);
+            rectangle.Print();
+
+            Square square = new Square(30);
+            square.Print();
+
+            Circle circle = new Circle(10);
+
+            circle.Print();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Laba2/Rectangle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace Figures
+{
+    public class Rectangle : Figure, IPrint
+    {
+        private double rect_height;
+        private double rect_width;
+        public double height
+        {
+            get
+            {
+                return this.rect_height;
+            }
+            set
+            {
+                this.rect_height = value;
+            }
+        }
+        public double width
+        {
+            get
+            {
+                return this.rect_width;
+            }
+            set
+            {
+                this.rect_width = value;
+            }
+        }
+
+        public Rectangle(double height, double width)
+        {
+            this.height = height;
+            this.width = width;
+            this.Type = "Прямоугольник";
+        }
+
+        public override double Area()
+        {
+            return this.width * this.height;
+        }
+
+        public override string ToString()
+        {
+            return "Фигура - " + this.Type + " h = " + height + " w = " + width + " S =  " + this.Area().ToString();
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+}
