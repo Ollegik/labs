51ca40c921dd2bce07bcc246eff0c0b4ba6a35dc
Ollegik <oleg.kozinov@mail.ru>
2019-12-13 22:22:01 +0300

    lab6

--- Lab2/Laba22/Main.cs
+++ Lab2/Laba22/Main.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using Figures;
+using System.Text;
+using System.Linq;
 namespace Lab2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            
             Console.Title = "Козинов Олег ИУ5-35Б";
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
 
             Rectangle rectangle = new Rectangle(10, 20);
             rectangle.Print();
@@ -19,6 +25,11 @@ namespace Lab2
             circle.Print();
 
             Console.ReadLine();
+            
+
+          
         }
     }
+   
 }
+
---
+++ Lab6/Lab6/AT.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Lab6
+{
+    [AttributeUsage(AttributeTargets.Property,
+    AllowMultiple = false,
+    Inherited = false)
+    ]
+    public class AT : Attribute
+    {
+        public AT() { }
+        public AT(string mDescription)
+        {
+            Description = mDescription;
+        }
+        public string Description { get; set; }
+    }
+}
